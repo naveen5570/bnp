ae7375dc7670b8790629264694a54fa967304075
instrovate <lms@instrovate.com>
2022-03-18 00:50:49 +0530

    new

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,14 @@
 import initDB from '../../helpers/initDB'
 import Product from '../../models/Product'
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'test' })
+export default async(req, res) => {
+    try{
+        //console.log('test=>'+req.query.name);
+        //var query = {name:req.query.name};
+      const products =  await Product.find()
+      res.status(200).send(products)
+    }catch(err){
+      console.log(err)
+    }
   }
   

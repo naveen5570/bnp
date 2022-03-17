b1bc3ba4d2c572d638331a69128af65582ba386c
instrovate <lms@instrovate.com>
2022-03-18 00:34:31 +0530

    new

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import initDB from '../../helpers/initDB'
+initDB()
 type Data = {
   name: string
 }

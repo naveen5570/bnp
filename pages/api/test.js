d9793609457b597db307e57870aa333d306339ac
instrovate <lms@instrovate.com>
2022-03-18 00:38:01 +0530

    new

---
+++ pages/api/test.js
@@ -0,0 +1,7 @@
+import initDB from '../../helpers/initDB'
+import Product from '../../models/Product'
+
+export default function handler(req, res) {
+    res.status(200).json({ name: 'John Doe' })
+  }
+  
